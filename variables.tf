19631663b2f076663d0b03412c05fb7737cccc5d
japcio <japcio@gmail.com>
2022-07-07 14:41:41 +0200

    New resources

--- variables.tf
+++ variables.tf
@@ -5,3 +5,25 @@ variable "client_secret" {
     type = string
     default = ""
 }
+
+
+variable "ARM_CLIENT_ID" {
+    type = string
+    default = ""
+}
+  
+variable "ARM_CLIENT_SECRET" {
+    type = string
+    default = ""
+}
+  
+variable "ARM_SUBSCRIPTION_ID" {
+    type = string
+    default = ""
+}
+  
+variable "ARM_TENANT_ID" {
+    type = string
+    default = ""
+}
+ 
